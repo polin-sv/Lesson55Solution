76fa883b3d526c59ff8e48e302dc3bf041d7f3b7
polin-sv <polinasvetlay@icloud.com>
2026-04-13 16:58:33 +0300

    Add project files.

---
+++ task01/logic.h
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+
+int get_average(int arrry[], int size);
+string get_best_class(int array1[], int arrry2[], int arrry3[], int size);
---
+++ task01/main.cpp
@@ -0,0 +1,10 @@
+#include "logic.h"
+
+int main() {
+	int size;
+	cout << "input number of people: ";
+	cin >> size;
+
+
+	return 0;
+}
